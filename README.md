4d63346b434c429f9b03bbc92df98e59f2362e3e
Matthias Mayer <matthias.mayer@tum.de>
2024-03-15 12:05:55 +0100

    add evaluation scripts + plotting results + raw csv data in zip

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 Robot Base Pose Optimization
 ============================
 
-This repository contains all codes required to run and analyze the experiments of the paper:
+This repository contains all the code required to run and analyze the experiments of the paper:
 
-"Decreasing Robotic Cycle Time via Base-Pose Optimization", by Matthias Mayer and Matthias Althoff
+"Decreasing Robotic Cycle Time via Base-Pose Optimization", by Matthias Mayer and Matthias Althoff, 2024.
+
+Feel free to send an [E-Mail](mailto:matthias.mayer@tum.de?subject=BaseOptPaper) to receive a preprint of the paper.
 
 Install
 =======
 
+This repository uses [git submodules](https://git-scm.com/book/en/v2/Git-Tools-Submodules) and [git lfs](https://git-lfs.com/).
+It is easiest if you install git lfs first, e.g., on Ubuntu `(sudo) apt-get install git-lfs`.
+Then clone the repository with `git clone --recurse-submodules <repo link>`.
+Check if the csv files in the [data](data/outdir_test_1200/outdir_hard_1200/Space/xyz/Set/test_hard/Alg/BOOptimizer/Seed/1_raw.csv) folder are not empty (otherwise lfs failed) and that [mcs](mcs) is non-empty (otherwise git submodules failed).
+
 0. We suggest setting up a conda environment for better control and isolation: `conda create --name rbpo python==3.11`.
-1. Then install the requirements: `pip install -r requirements.txt`. We install this package and mcs in editable mode, so that changes to the code are immediately reflected in the installed package.
+Activate the environment with `conda activate rbpo`.
+1. Then install the requirements: `pip install -r requirements.txt`.
+We install this package and [mcs](https://gitlab.lrz.de/tum-cps/mcs) (providing utilities such as path planning) in editable mode, so that changes to the code are immediately reflected in the installed package.
 
 Running the Experiments
 =======================
 
-The experiments are best run inside a docker container. Use the provided docker-compose file to get a shell with all dependencies installed: `docker compose run -i --service-ports base-opt bash`.
+The experiments are best run inside the provided docker container.
+Use the provided docker-compose file to get a shell with all dependencies installed: `docker compose run -i --service-ports base-opt bash`.
 Then you can run the experiments with the provided shell scripts in the `experiments` folder.
 The experiments have two parts:
 
@@ -22,16 +32,18 @@ The experiments have two parts:
 The optuna database for our training is given in [data/optuna.db](data/optuna.db).
 It can be viewed, e.g., with the optuna dashboard: `optuna-dashboard --port 6006 sqlite:///data/optuna.db`.
 2. Evaluation of the best hyperparameters [run_evaluation.sh](experiments/run_evaluation.sh)
+We suggest running the evaluation with [GNU parallel](https://www.gnu.org/software/parallel/) over task sets/action spaces/seed/algorithm combinations as done in [run_evaluation.sh](experiments/run_evaluation.sh).
+The main output are a set of csv files per task set, action space, algorithm, and seed combination, logs of errors/stdout per run are stored alongside.
+The results for our analysis are provided in the [data/outdir_test_1200 folder](data/outdir_test_1200).
 
 We note that the experiments are computationally expensive and require a lot of CPU time and memory (order of 1000s CPU hours).
-We suggest running the evaluation with [GNU parallel](https://www.gnu.org/software/parallel/) over task sets/action spaces/seed/algorithm combinations as done in [run_evaluation.sh](experiments/run_evaluation.sh).
-The results for the analysis are provided in the [data/outdir_test_1200 folder](data).
 
 Running the Evaluation
 ======================
 
-The evaluations for the paper are provided as a [jupyter notebook](tba).
+The evaluations for the paper are provided as a [jupyter notebook](evaluation/evaluate_all.ipynb).
 These can be run just by installing the requirements, starting jupyter lab `jupyter lab` and running the notebook.
+The notebook uses the csv files in [data/outdir_test_1200 folder](data/outdir_test_1200) to create all plots and tables used in the paper.
 
 Issue Tracker and Questions
 ===========================
@@ -43,8 +55,9 @@ Unittests and CI
 
 We use pytest for unittests and gitlab CI for continuous integration.
 The used docker image can be locally built with `ci/build_docker.sh`.
-To run the unittests via PyCharm make sure you set the working directory to the root of the repository.
+To run the unittests via PyCharm make sure you set the working directory to the root of the repository and we suggest using the docker compose interpreter to easily test the OMPL integrations.
 
 Contact
 =======
-Matthias Mayer (matthias.mayer@tum.de)
+
+Matthias Mayer [matthias.mayer@tum.de](mailto:matthias.mayer@tum.de)
---
+++ src/base_opt/utilities/eval_utils.py
@@ -0,0 +1,103 @@
+from typing import List
+
+import pandas as pd
+
+
+FAIL_REASON_ORDER = [
+    'Robot Length Filter',
+    'Simple IK Filter',
+    'IK Filter',
+    'Path planning failed',
+    'Path planning timeout',
+    'Invalid Path',
+    'No failure']
+
+
+def run_eval():
+    """Print out a suggestion to run the evaluation script with GNU parallel."""
+    print("Please look into experiments/run_evaluation.sh")
+
+
+def cleanup_preprocess_results(result_df: pd.DataFrame, group_by: List[str] = None) -> pd.DataFrame:
+    """
+    Clean up and preprocess the results.
+
+    * Turn 'Run Time' into pandas time
+    * Add 'Maximum Reward' column which is the maximum reward up to the current step
+    * Rename 'Unnamed: 0' to 'Step'
+    * Add 'Step Time' column which is the time taken from the previous step
+    * Add 'Success Till Step' column which is True if a valid solution was found up to the current step
+    * Rename 'Algorithm' to shorter names
+    * Clean up failure reasons
+    """
+    if group_by is None:
+        group_by = ['Algorithm', 'Task ID', 'Seed']
+
+    # Turn run-time into time
+    result_df['Run Time'] = pd.to_datetime(result_df['Run Time'], unit="s")
+    # Maximum reward up to step
+    result_df['Maximum Reward'] = result_df.groupby(group_by)['Reward'].cummax()
+    # Rename step
+    result_df = result_df.rename(columns={'Unnamed: 0': "Step"})
+    # Time per step
+    result_df['Step Time'] = result_df.groupby(group_by)['Run Time'].diff()
+    # Fill in first step assuming start at 0
+    result_df.loc[result_df['Step'] == 0, 'Step Time'] = result_df[result_df['Step'] == 0]['Run Time'] - pd.Timestamp(0)
+    result_df['Step Time'] += pd.Timestamp(0)  # Make plotable
+    # Success up to step
+    result_df['Success Till Step'] = result_df.groupby(group_by)['Valid Solution'].cummax()
+    # Rename algorithm
+    result_df['Algorithm'] = result_df['Algorithm'].replace({'BOOptimizer': 'BO',
+                                                             'GAOptimizer': 'GA',
+                                                             'RandomBaseOptimizer': 'Random',
+                                                             'DummyOptimizer': 'Dummy'})
+    # Clean up failure reasons
+    result_df['Fail Reason'] = result_df['Fail Reason'].str.replace(
+        r' \(<timor.task.Task.Task object at 0x[0-9a-f]*>\)', '', regex=True)  # Remove task object address
+    result_df['Fail Reason'] = result_df['Fail Reason'].str.replace(
+        r"'task': Task [A-Za-z0-9_/]+,", "'task': ..,", regex=True)  # Remove task id
+    result_df['Fail Reason'] = result_df['Fail Reason'].str.replace(
+        r"Unknown; info: \{'is_success': False.*\}", "Invalid path", regex=True)  # Remove solution for failed tasks
+    result_df['Fail Reason'] = result_df['Fail Reason'].replace({
+        "Failed filters: [InverseKinematicsSolvable({'ignore_self_collisions': True, 'max_iter': 300})]":
+            'Simple IK Filter',
+        "Failed filters: [RobotLongEnoughFilter]": "Robot Length Filter",
+        "Trajectory generation failed": "Path planning failed",
+        "Failed filters: [InverseKinematicsSolvable({'task': .., 'max_iter': 1500})]": "IK Filter",
+        "": "No failure",
+        "Path planning failed": "Path planning failed",
+        "Timeout task solver": "Path planning timeout",
+    })
+    return result_df
+
+
+def normalize_time(result_df: pd.DataFrame, sampling_time: str = '1000ms',
+                   group_by: List[str] = None) -> pd.DataFrame:
+    """Return resampled DataFrame with common frequency and padded end time."""
+    if group_by is None:
+        group_by = ['Algorithm', 'Task ID', 'Seed']
+    max_run_time = result_df['Run Time'].max()
+    final_dfs = []
+    for group_idx, group in result_df.groupby(group_by):
+        # Pad start
+        start_df = group[group['Step'] == 0].copy()
+        start_df['Run Time'] = pd.Timestamp(0)
+        start_df['Reward'] = start_df['Reward Fail']
+        start_df['Solution'] = None
+        start_df['Valid Solution'] = False
+        start_df['Fail Reason'] = ''
+        start_df['Success Till Step'] = False
+        # Pad end
+        end_df = group[group['Step'] == group['Step'].max()].copy()
+        end_df['Run Time'] = max_run_time + pd.Timedelta(1, unit="ms")
+        padded_df = pd.concat([start_df, group, end_df])
+        final_dfs.append(padded_df.set_index('Run Time').resample(sampling_time).ffill().reset_index())
+    return pd.concat(final_dfs, axis=0, ignore_index=True)
+
+
+def print_step_count(result_df):
+    """Print step count statistics per algorithm."""
+    final_step_count_per_trial = result_df.groupby(['Algorithm', 'Task ID', 'Seed'])['Step'].max()
+    step_count_stats = final_step_count_per_trial.groupby(['Algorithm']).aggregate(['mean', 'max', 'min'])
+    print("Step count statistics:")
+    print(step_count_stats)
