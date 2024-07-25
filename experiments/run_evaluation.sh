a5d6a3bef16df536704868324fc6bae07034e680
Matthias Mayer <matthias.mayer@tum.de>
2024-07-25 12:55:43 +0200

    Add evaluation command for extended task set + add ctrl+c handler for evaluate algorithm

--- experiments/run_evaluation.sh
+++ experiments/run_evaluation.sh
@@ -20,3 +20,15 @@ parallel --bar --header : --results data/outdir_test_1200/outdir_hard_1200 -j 40
   --solution-storage data/solutions/ \
   --timeout 1200 \
   --reward-fail -50 ::: Space xyz xyz_rotvec ::: Set test_hard test_realworld ::: Alg BOOptimizer GAOptimizer RandomBaseOptimizer DummyOptimizer ::: Seed {1..5} > /dev/null
+
+# Edge-case tasks (10 types x 2 difficulties x 2 actions x 5 seeds x 4 algorithms = 800 runs a 1200 sec = ~266,67 h)
+parallel --bar --header : --results data/outdir_test_1200/outdir_edge_1200 -j 40 python src/base_opt/base_opt/evaluate_algorithm.py \
+  --eval-set test_edge_case \
+  --algorithm {Alg} \
+  --action-space {Space} \
+  --cost-function cyc \
+  --seed {Seed} \
+  --raw-data data/outdir_test_1200/outdir_edge_1200/Space/{Space}/Alg/{Alg}/Seed/{Seed}_raw.csv \
+  --solution-storage data/solutions/ \
+  --timeout 1200 \
+  --reward-fail -50 ::: Space xyz xyz_rotvec ::: Alg BOOptimizer GAOptimizer RandomBaseOptimizer DummyOptimizer ::: Seed {1..5} > /dev/null
