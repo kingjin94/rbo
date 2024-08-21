d86434e08933212b9b892b3f12fe4ea408dd5125
Matthias Mayer <matthias.mayer@tum.de>
2024-08-21 10:09:52 +0200

    Update evaluation scripts

--- experiments/run_evaluation.sh
+++ experiments/run_evaluation.sh
@@ -7,7 +7,7 @@ parallel --bar --header : --results data/outdir_test_1200/outdir_simple_1200 -j 
   --seed {Seed} \
   --raw-data data/outdir_test_1200/outdir_simple_1200/Space/{Space}/Alg/{Alg}/Seed/{Seed}_raw.csv \
   --solution-storage data/solutions/ \
-  --timeout 1200 ::: Space xyz xyz_rotvec ::: Alg BOOptimizer GAOptimizer RandomBaseOptimizer DummyOptimizer ::: Seed {1..5} > /dev/null
+  --timeout 1200 ::: Space xyz xyz_rotvec ::: Alg BOOptimizer GAOptimizer RandomBaseOptimizer DummyOptimizer AdamOptimizer ::: Seed {1..5} > /dev/null
 
 # Hard/Real-world test set evaluation (total 127 * 2 * 4 * 5 = 5080 runs a 240 sec = ~339 h)
 parallel --bar --header : --results data/outdir_test_1200/outdir_hard_1200 -j 40 python src/base_opt/base_opt/evaluate_algorithm.py \
@@ -19,7 +19,7 @@ parallel --bar --header : --results data/outdir_test_1200/outdir_hard_1200 -j 40
   --raw-data data/outdir_test_1200/outdir_hard_1200/Space/{Space}/Set/{Set}/Alg/{Alg}/Seed/{Seed}_raw.csv \
   --solution-storage data/solutions/ \
   --timeout 1200 \
-  --reward-fail -50 ::: Space xyz xyz_rotvec ::: Set test_hard test_realworld ::: Alg BOOptimizer GAOptimizer RandomBaseOptimizer DummyOptimizer ::: Seed {1..5} > /dev/null
+  --reward-fail -50 ::: Space xyz xyz_rotvec ::: Set test_hard test_realworld ::: Alg BOOptimizer GAOptimizer RandomBaseOptimizer DummyOptimizer AdamOptimizer ::: Seed {1..5} > /dev/null
 
 # Edge-case tasks (10 types x 2 difficulties x 2 actions x 5 seeds x 4 algorithms = 800 runs a 1200 sec = ~266,67 h)
 parallel --bar --header : --results data/outdir_test_1200/outdir_edge_1200 -j 40 python src/base_opt/base_opt/evaluate_algorithm.py \
@@ -32,4 +32,4 @@ parallel --bar --header : --results data/outdir_test_1200/outdir_edge_1200 -j 40
   --solution-storage data/solutions/ \
   --timeout 1200 \
   --task-solver-timeout 30 \
-  --reward-fail -50 ::: Space xyz xyz_rotvec ::: Alg BOOptimizer GAOptimizer RandomBaseOptimizer DummyOptimizer ::: Seed {1..5} > /dev/null
+  --reward-fail -50 ::: Space xyz xyz_rotvec ::: Alg BOOptimizer GAOptimizer RandomBaseOptimizer DummyOptimizer AdamOptimizer ::: Seed {1..5} > /dev/null
